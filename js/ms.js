8239e4df516886d3e28588b7200f3a0f8c452e8a
iwin88789 <134831098+iwin88789@users.noreply.github.com>
2023-06-11 16:45:31 +0700

    first commit

--- js/ms.js
+++ js/ms.js
@@ -1,11 +1,10 @@
 
-var urlList = ['Hay88.plus','Hay88.one', 'Hay88.mobi', 'Hay88.fun',
-	'Hay88.net',
-	'hay88.org' 
+var urlList = ['hay8811.com','hay8822.com', 'hay8833.com', 'hay8844.com',
+	'hay8855.com', 'hay8866.com', 'hay8877.com', 'hay8888.com', 'hay8899.com'
 ];
 
 function urlli() {
-	for (i = 0; i <= 5; i++) {
+	for (i = 0; i <= 9; i++) {
 		$('.ulList li').eq(i).find('a').attr("href", 'https://www.'+urlList[i]);
 		// $('.ulList li').eq(i).find('span.url').html(urlList[i]);
 		$('.ulList li').eq(i).find('a.url').find('span').html(urlList[i]);
@@ -29,5 +28,14 @@ setTimeout(function() {
 }, 500)
 setTimeout(function() {
 	$('#lineMs5').html(Math.ceil(Math.random() * 70) + 'ms')
+}, 600)
+setTimeout(function() {
+	$('#lineMs6').html(Math.ceil(Math.random() * 100) + 'ms')
+}, 600)
+setTimeout(function() {
+	$('#lineMs7').html(Math.ceil(Math.random() * 50) + 'ms')
+}, 600)
+setTimeout(function() {
+	$('#lineMs8').html(Math.ceil(Math.random() * 60) + 'ms')
 }, 600)
 
